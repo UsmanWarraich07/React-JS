94351925833696b7183a74f2206663cdcef246f5
UsmanWarraich07 <musmanwarraich238@gmail.com>
2024-03-15 17:46:40 +0500

    update code

--- 04-eat-n-split/src/App.js
+++ 04-eat-n-split/src/App.js
@@ -106,12 +106,12 @@ function Friend({ friend, onSelection, selectedFriend }) {
       <h3>{friend.name}</h3>
       {friend.balance < 0 && (
         <p className="red">
-          You owe {friend.name} {Math.abs(friend.balance)}
+          You owe {friend.name} {Math.abs(friend.balance)}$
         </p>
       )}
       {friend.balance > 0 && (
         <p className="green">
-          {friend.name} owe you {Math.abs(friend.balance)}
+          {friend.name} owe you {Math.abs(friend.balance)}$
         </p>
       )}
       {friend.balance === 0 && <p>You and {friend.name} are even.</p>}
